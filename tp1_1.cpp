3786ec54942c367e934a53185459a76db9dbe859
FranDev <fran.cordoba.developer@gmail.com>
2024-03-26 16:32:26 -0300

    punto 3 completo

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,7 +3,18 @@
 
 int main ()
 {
-    printf("Hola mundo");
+    
+    int numero = 26;
+    int *punt_numero = &numero;
 
+    printf("\nContenido del puntero: %d\n", *punt_numero);
+
+    printf("\nDirección de memoria almacenada por el puntero: %p\n", (void*) punt_numero);
+
+    printf("\nDireccion de memoria del puntero: %p", &punt_numero);
+
+    printf("\nDireccion de memoria de la variable: %p", &numero);
+
+    printf("\n Tamaño de memoria utilizado por la variable: %zu bytes\n", sizeof(numero));
     return 0;
 }
